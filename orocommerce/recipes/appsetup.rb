c88a41f784892df8559459474fc476ab24477f4b
Wikus Verreynne <wikus.verreynne@gmail.com>
2017-03-31 20:50:01 +0200

    change php version to 5.6

--- orocommerce/recipes/appsetup.rb
+++ orocommerce/recipes/appsetup.rb
@@ -8,6 +8,19 @@ node[:deploy].each do |app_name, deploy|
     action :install
   end
 
+  apt_repository 'php56' do
+    uri 'ppa:ondrej/php5-5.6'
+    action :add
+  end
+
+  apt_update 'update' do
+    action :update
+  end
+
+  apt_package 'php5' do
+    action :install
+  end
+    
   script "install_composer" do
     interpreter "bash"
     user "root"
