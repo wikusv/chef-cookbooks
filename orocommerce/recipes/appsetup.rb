f52bee50919894f63f32ba8e743c3dffddb1f4fc
Wikus Verreynne <wikus.verreynne@gmail.com>
2017-03-31 21:49:21 +0200

    no message

--- orocommerce/recipes/appsetup.rb
+++ orocommerce/recipes/appsetup.rb
@@ -8,24 +8,19 @@ node[:deploy].each do |app_name, deploy|
     action :install
   end
 
-  apt_repository 'php56' do
-    uri 'ppa:ondrej/php5-5.6'
-    action :add
-  end
 
-  apt_update 'update' do
-    action :update
-  end
-
-  apt_package 'php5' do
-    action :install
-  end
-    
   script "install_composer" do
     interpreter "bash"
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
+
+    add-apt-repository ppa:ondrej/php
+    apt-get update
+    apt-get purge php5-common # remove and purge old PHP 5.x packages
+    apt-get update
+    apt-get install libapache2-mod-php5.6
+
     curl -s https://getcomposer.org/installer | php
     composer global require "fxp/composer-asset-plugin:~1.2"
     php composer.phar install --prefer-dist
